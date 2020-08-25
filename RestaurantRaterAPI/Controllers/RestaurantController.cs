b5e193e2851070a0450897fa5e15b487d6703e25
seansql <shawnq2116@gmal.com>
2020-08-25 15:10:05 -0400

    Built out GET methods

--- RestaurantRaterAPI/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterAPI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,12 +12,18 @@ namespace RestaurantRaterAPI.Controllers
 {
     public class RestaurantController : ApiController
     {
-        RestaurantDbContext _context = new RestaurantDbContext();
+        private readonly RestaurantDbContext _context = new RestaurantDbContext(); // we set a field
 
         // all endpoints will be public
         //-- Create (Post)
-        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        [HttpPost]
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model) // if we want to change PostRestaurant to CreateRestaurant, the only way to make sure that this method is set as a post method is to add attribute [HttpPost]
         {
+            if (model is null) // == works the same
+            {
+                return BadRequest("Your request body cannot be empty.");
+            }
+
             if (ModelState.IsValid) // modelstate property is inside apicontroller
             {
                 _context.Restaurants.Add(model);
@@ -30,8 +37,19 @@ namespace RestaurantRaterAPI.Controllers
 
         //-- Read (Get)
         // Get by ID
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            return Ok();
+        }
 
         // Get All
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync(); // Async methods usually need await in front
+            return Ok(restaurants);
+        }
 
         //-- Update (Put)
 
